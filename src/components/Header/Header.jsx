49ccdab34b92fe3c267f420575cfd7a4b8f3e76e
Kalaivathanan1997 <kalaivathanan4@gmail.com>
2024-04-01 21:27:47 +0530

    FEAT: create food delivery frontend and set layouts and home page initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+        <h2>Order Your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea accusantium, id dolores earum laudantium esse. Amet molestias ab perspiciatis dolorem assumenda exercitationem alias, numquam saepe.</p>
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
